3bdfa6d682b08102ac823e95fbacfb6c72b94d28
Jonah Bloch-Johnson <me@jonah.org>
2025-05-29 09:52:28 -0400

    start with first video

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   redirects: {
     "/act": "https://5calls.org/issue/climate-weather-research-funding-cuts/",
     "/watch": "https://www.youtube.com/@wclivestream/live",
-    "/catchup": "https://www.youtube.com/playlist?list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_"
+    "/catchup": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_"
   },
   site: "https://foxi.netlify.app/",
   integrations: [
