1a55cffe5b4e1061e441696e96cfcf9d451ea9a0
Jonah Bloch-Johnson <me@jonah.org>
2025-06-01 13:24:38 -0400

    tumblr

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,9 @@ export default defineConfig({
     "/act": "https://5calls.org/issue/climate-weather-research-funding-cuts/",
     "/watch": "https://www.youtube.com/@wclivestream/live",
     "/live": "https://www.youtube.com/@wclivestream/live",
-    "/catchup": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_"
+    "/catchup": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_",
+    "/tumblr": "https://www.tumblr.com/communities/weather-n-climate-livestreams",
+    "/blorbo": "https://www.tumblr.com/communities/weather-n-climate-livestreams"
   },
   site: "https://foxi.netlify.app/",
   integrations: [
