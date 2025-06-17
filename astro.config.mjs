558611e81cfe96f8b7b6ff8f5f5f28dd94eaddb0
Jonah Bloch-Johnson <me@jonah.org>
2025-06-18 01:13:21 +0200

    watch goes to channel page

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   redirects: {
     "/act": "https://5calls.org/issue/climate-weather-research-funding-cuts/",
-    "/watch": "https://www.youtube.com/@wclivestream/live",
-    "/live": "https://www.youtube.com/@wclivestream/live",
+    "/watch": "https://www.youtube.com/@wclivestream",
+    "/live": "https://www.youtube.com/@wclivestream",
     "/catchup": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_",
     "/tumblr": "https://www.tumblr.com/communities/weather-n-climate-livestreams",
     "/blorbo": "https://www.tumblr.com/communities/weather-n-climate-livestreams",
