bfec539c7fb955f880664110542fec809cb9ce5f
Jonah Bloch-Johnson <me@jonah.org>
2025-12-19 16:41:59 -0500

    link

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    "/act": "https://5calls.org/issue/climate-weather-research-funding-cuts/",
+    "/act": "https://5calls.org/issue/ncar-national-center-for-atmospheric-research/",
     "/watch": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_",
     "/live": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_",
     "/catchup": "https://www.youtube.com/watch?v=CcWR6CdJnvQ&list=PLDDEU7mKb6z0RD8V9eXw8TH_qJ1Jtb5u_",
